9e9054a1caaeb74213bfd05151d5c1d44d4bd76c
RizkiAndrianto <rizki_avenzho@yahoo.com>
2019-09-25 00:44:18 +0700

    Update Spawn + Move

---
+++ PilahMan/New Unity Project/Assets/Scripts/Spawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject[] rubish;
+    public Vector3 spawnpos;
+    public float nextspawn = 0f;
+    public float spawnrate = 2f;
+    public bool stop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(nextspawn);
+
+        while (!stop)
+        {
+            Vector3 screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), 1));
+            Instantiate(rubish[0], screenPosition, Quaternion.identity);
+
+            yield return new WaitForSeconds(spawnrate);
+        }
+
+    }
+}
